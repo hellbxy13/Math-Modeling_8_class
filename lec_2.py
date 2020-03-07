2baf44bd5a8953c04c4828bd57703583f9139c59
hellbxy13 <61882856+hellbxy13@users.noreply.github.com>
2020-03-07 11:34:06 +0200

    Add files via upload

---
+++ lec_2.py
@@ -0,0 +1,66 @@
+if 1:
+    print ('hello 1')
+    
+    
+a=3
+if a>1:
+    print ('hello 3')
+    
+    
+if a>=3:
+    print ('hello 3')
+    
+    
+b=5
+if b==5:
+    print('hello wrld')
+    
+    
+a=3
+if a>4:
+    print ('hello 4')
+else:
+    print ('hello a')
+    
+a=5
+if a>4:
+    print ('hello 4')
+else:
+    print ('hello a')
+    
+
+a=3
+if a>5:
+    print ('i am gay')
+elif a<2:
+    print ('you are gay')
+else:
+    print ('gay')
+    
+a=3
+b=4
+c=5
+if a>3 and b==2:
+    print('good')
+elif b>3 or c==5:
+    print('best')
+else:
+    print('bud')
+    
+for i in 1,3,4:
+    print (i**2, end=' ')
+    print (i**3, sep=' ')
+ 
+a=[1,5,7,10]
+for g in a:
+    print(g**4, end=' yes ')
+
+
+
+
+for i in range(0, 10, 1): 
+    print(i, end=' ')
+
+
+
+
